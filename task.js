ec636fb41017cd41eb59a7b7d88e55b4faac24b3
Pratheek0599 <pratheek0599@gmail.com>
2024-06-03 16:13:59 +0530

    first commit

---
+++ task.js
@@ -0,0 +1,56 @@
+let myResume = {
+  basics: {
+    name: "Pratheek",
+    email: "[pratheek0599@gmail.com",
+    phone: 9629977335,
+    degree: "B.sc",
+    location: {
+      address: "4/10A, suleeswaranpatti, Pollachi",
+      postalCode: 642006,
+      city: "Pollachi",
+      state: "Tamilnadu",
+      country: "India",
+    },
+    profiles: [
+      {
+         github: "https://github.com/Pratheek0599",
+      },
+    ],
+  },
+  work: [
+    {
+      company: "Focus Edumatics",
+      position: "Online Tutor",
+      startDate: "2020-11-04",
+      endDate: "2021-08-15",
+      
+    },
+  ],
+  education: [
+    {
+      institution: "Nehru arts and science college",
+      studyType: "fulltime",
+      "batch start year": 2015,
+      "batch end year": 2019,
+      gpa: 7.5,
+    },
+  ],
+  skills: [
+    {
+      name: "python,javascript",
+      level: "beginer",
+      project: ["automatic attendance maintaing system using python language"],
+    },
+  ],
+  languages: [
+    {
+      language: "Tamil,Enlish",
+    },
+  ],
+  interests: [
+    {
+      name: "script writter,youtuber,",
+    },
+  ],
+};
+console.log(myResume);
